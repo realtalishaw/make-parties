9ee70218a3f638fc19f4fabc7ad5538d3d511b8a
Talisha <misstalishawhite@gmail.com>
2021-06-06 11:46:16 -0500

    users can see all mock events

--- app.js
+++ app.js
@@ -18,11 +18,22 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecu
 //use handlebars to render
 app.set('view engine', 'handlebars');
 
+// MOCK ARRAY OF EVENTS FOR TESTING
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+]
+
 // Routes
+
+// INDEX ROUTE - SHOW ALL EVENTS
 app.get('/', (req, res) => {
-    res.render('home', { msg: "Handlebars are cool!" });
+    res.render('events-index', { events: events });
 })
 
+
+// PORT
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
